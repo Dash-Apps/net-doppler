bef6babd2be6818099e577276e3e52e53c1f8617
Rithvik Thummalapenta <102890515+rithvik-709@users.noreply.github.com>
2024-11-14 09:47:18 -0600

    Update s2s.py
    added code to ping_all and ping functions

--- s2s.py
+++ s2s.py
@@ -6,10 +6,20 @@ import protocols
 supported_protocols = ["CAN,UDP,TCP"]
 #ping a single host and wait for response
 def ping(protocol,host):
-    pass
+    icmp_packet = IP(dst=host) / ICMP()
+    sr1(icmp_packet)
+    response = sr1(icmp_packet, timeout=1)
 #discover and ping all hosts running on a server
 def ping_all():
-    pass
+    request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip)
+
+    ans, unans = srp(request, timeout=2, retry=1)
+    result = []
+
+    for sent, received in ans:
+        result.append({'IP': received.psrc, 'MAC': received.hwsrc})
+
+    return result
 
 #check what protocols are allowed for the target server
 def check_protocols():
