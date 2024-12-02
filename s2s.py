73ab9dcbef17508c9554090dcefb1b01fddb6ddf
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-12-02 21:01:59 +0000

    added functioning packet sniff function

--- s2s.py
+++ s2s.py
@@ -1,12 +1,12 @@
-from scapy.all import *
+import scapy.all as scapy
 import protocols
 import itertools
 #functions to allow for, and gather data important to, server to server communication
 supported_protocols = ["CAN,UDP,TCP"]
 #ping a single host and wait for response
 def ping(host):
-    request = IP(dst=host)/TCP(dport=[21,22,23])
-    res,unans = sr(request) 
+    request = scapy.IP(dst=host)/scapy.TCP(dport=[21,22,23])
+    res,unans = scapy.sr(request) 
     return res,unans
 
 def ping_multiple(target_list):
@@ -26,4 +26,14 @@ def ping_all(target,range):
 #check what protocols are allowed for the target server
 def check_protocols():
     pass
+def packet_sniff(amount=0):
+    #if no amount of packets is entered, sniff forever until the loop is broken.
+    if amount == 0:
+        while True:
+            scapy.sniff(count=1).summary()
+    #otherwise sniff for the amount specified
+    else:
+        scapy.sniff(count=amount).summary()
+    #sniffs one packet at a time for display purposes when running infinitely
 
+packet_sniff(amount=10)
